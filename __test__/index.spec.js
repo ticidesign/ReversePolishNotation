3fac541c87041ae02e226a5846e1ea10b5f05b38
ticidesign <ticipp@gmail.com>
2018-11-07 19:52:19 +1100

    Fixing utils

--- __test__/index.spec.js
+++ __test__/index.spec.js
@@ -1,4 +1,47 @@
-const { resolvePostfix, rpn, makeCoordinates } = require('../index.js');
+const {
+  isNumber,
+  isCoordenates,
+  isOperator,
+  resolvePostfix,
+  rpn,
+  makeCoordinates,
+} = require('../index.js');
+
+test('is a Number', () => {
+  expect(isNumber(20)).toBe(true);
+  expect(isNumber(0.01)).toBe(true);
+  expect(isNumber('10')).toBe(true);
+  expect(isNumber('-3')).toBe(true);
+  expect(isNumber('3')).toBe(true);
+})
+
+test('is NOT a Number', () => {
+  expect(isNumber('47uiu')).toBe(false);
+  expect(isNumber('0-0')).toBe(false);
+  expect(isNumber('O')).toBe(false);
+})
+
+test('is Coordenates', () => {
+  expect(isCoordenates('z8')).toBe(true)
+  expect(isCoordenates('abc123')).toBe(true)
+})
+test('is NOT a Coordenates', () => {
+  expect(isCoordenates('8')).toBe(false)
+  expect(isCoordenates('abc')).toBe(false)
+})
+
+test('is an Operator', () => {
+  expect(isOperator.includes('+')).toBe(true)
+  expect(isOperator.includes('-')).toBe(true)
+  expect(isOperator.includes('*')).toBe(true)
+  expect(isOperator.includes('/')).toBe(true)
+})
+test('is NOT an Operator', () => {
+  expect(isOperator.includes('+')).toBe(true)
+  expect(isOperator.includes('-')).toBe(true)
+  expect(isOperator.includes('*')).toBe(true)
+  expect(isOperator.includes('/')).toBe(true)
+})
 
 test('Resolve Postfix Notation - Douple spaces', () => {
   expect(resolvePostfix('5  6 +', 'aq20', {})).toBe(11);
@@ -29,6 +72,7 @@ test('Resolve Postfix Notation - No numbers', () => {
 test('Resolve Postfix Notation - Just one coordinate', () => {
   expect(resolvePostfix('z8', 'a1', {z8: '10'})).toBe(10);
 })
+
 test('Resolve Postfix Notation - With missing cordinate', () => {
   expect(resolvePostfix('c2 3 * ', 'a1', {a1: 'c2 3 * ', a2: '2'})).toBe('ERR!');
 })
@@ -39,12 +83,12 @@ test('Resolve Postfix Notation - With cordinate', () => {
 
 test('Resolve Postfix Notation - With many cordinates', () => {
   const COORD = {
-    z7: 'x8 3 +',
-    x8: 'z9 y7 *',
+    z7: 'x8 3 +', // 723
+    x8: 'z9 y7 *', // 720
     y7: '9',
-    z9: 'x10',
-    x10: 'x7 20 15 + +',
-    x7: '5 9 *', //45
+    z9: 'x10', // 80
+    x10: 'x7 20 15 + +', // 80
+    x7: '5 9 *', // 45
   };
   expect(resolvePostfix('z7 8 +', 'y8', COORD)).toBe(731);
 })
@@ -84,23 +128,35 @@ test('makeCoordinates - Should give us the right coordinates', () => {
 });
 
 test('RPN function', () => {
-  const input = 'b1 b2 +,2 b2 3 * -, ,+\na1     ,5         , ,7 2 /\nc2 3 * ,1 2       , ,5 1 2 + 4 * + 3 -';
-  const output = '-8,-13,ERR!,ERR!,-8,5,ERR!,3.5,ERR!,ERR!,ERR!,14'
+  // const COORD = {
+  //   a1: 'b1 b2 +', //-8
+  //   b1: '2 b2 3 * -', //-13
+  //   c1: ' ', //ERR!
+  //   d1: '+', //ERR!
+  //   a2: 'a1     ', //-8
+  //   b2: '5         ', //5
+  //   c2: ' ', //ERR!
+  //   d2: '7 2 /',  //3.5
+  //   a3: 'c2 3 * ',  //ERR!
+  //   b3: '1 2       ', //ERR!
+  //   c3: ' ', //ERR!
+  //   d3: '5 1 2 + 4 * + 3 -' //14
+  // }
+  const input = 'b1 b2 +,2 b2 3 * -, ,+\n' +
+                'a1     ,5         , ,7 2 /\n' +
+                'c2 3 * ,1 2       , ,5 1 2 + 4 * + 3 -';
+  const output = '-8,-13,ERR!,ERR!\n' +
+                '-8,5,ERR!,3.5\n' +
+                'ERR!,ERR!,ERR!,14';
   expect(rpn(input)).toBe(output);
 })
 
-test.only('avoid infinite loops', () => {
-  const input = '7, c2 40 +, +\nb1, a2 72 /, b3\n50, b2 7 8 + +';
-  const output = '7,ERR!,ERR!\n,ERR!,ERR!,ERR!\n50,ERR!,ERR!';
-  expect(rpn(input)).toBe(output);
-})
-
-
-// infinte loop example
-// 7, C2 40 +, +
-// B1, A2 72 /, B3
-// 50, B2 7 8 + +, ..
-
-//  7, ERR!, ERR!
-// , ERR!, ERR!, ERR!
-// 50, ERR!, ERR!
+// test('avoid infinite loops', () => {
+//   const input = '7, c2 40 +, +\n' +
+//                 'b1, a2 72 /, b3\n' +
+//                 '50, b2 7 8 + +, 8 +';
+//   const output = '7,ERR!,ERR!\n' +
+//                 'ERR!,ERR!,ERR!\n' +
+//                 '50,ERR!,ERR!';
+//   expect(rpn(input)).toBe(output);
+// })
--- index.js
+++ index.js
@@ -1,30 +1,43 @@
+const isOperator = ['+', '-', '/', '*'];
+// const isNumber = new RegExp('^[0-9]+');
+// const isNumber = (item) => {
+//   const regexNumber = new RegExp('^[-]?[0-9]\d*(\.\d+)?$');
+//   return regexNumber.test(item);
+// }
+const isNumber = ( item ) => {
+  return ( parseFloat( item ) - parseFloat( item ) + 1 ) === 1 &&
+  String( parseFloat( item ) ).length === String( item ).length;
+}
+const isCoordenates = (item) => {
+  const regexItem = new RegExp('^[a-zA-Z]+[0-9]+$');
+  return regexItem.test(item);
+}
+
 const resolvePostfix = (input, address, COORD, stacktrace = []) => {
+
   if (!input) return 'ERR!';
 
-  const isOperator = ['+', '-', '/', '*'];
-  const isNumber = new RegExp('^[0-9]+$');
-  const isCoordenates = new RegExp('^[a-zA-Z]+[0-9]+$');
-  let stack = [];
+  const stack = [];
 
   // Creating a stack tracing to avoid infinite loop
-  if (stacktrace.includes(address)) return "ERR!";
-  stacktrace.push(address);
+  // if (stacktrace.includes(address)) return "ERR!";
+  // stacktrace.push(address);
 
   input
     .replace(/\s\s+/g, ' ') //replace all space by single space
     .split(' ')
     .forEach(item => {
-      if (isCoordenates.test(item)) {
-        const solution = resolvePostfix(COORD[item], address, COORD, stacktrace);
+      if (isCoordenates(item)) {
+        const solution = resolvePostfix(COORD[item], item, COORD, stacktrace);
         stack.push(solution);
       }
-      if (isNumber.test(item)) {
-        stack.push(Number(item));
+      if (isNumber(item)) {
+        stack.push(parseInt(item));
       }
       if (isOperator.includes(item)) {
-        if (stack.length === 0) return null;
-        let b = Number(stack.pop());
-        let a = Number(stack.pop());
+        // if (stack.length === 0) return null;
+        const b = stack.pop();
+        const a = stack.pop();
         let calculation;
         if(item === '+') {
           calculation = a + b;
@@ -45,11 +58,11 @@ const resolvePostfix = (input, address, COORD, stacktrace = []) => {
           stack.push(calculation);
         }
       }
-    })
-
+    });
+    // console.log(stack, address, stacktrace);
     if (stack.length === 1) return stack.pop();
     return "ERR!";
-}
+};
 
 const makeCoordinates = ( csv ) => {
   const result = {};
@@ -68,44 +81,42 @@ const makeCoordinates = ( csv ) => {
 };
 
 const numberToLetter = (n) => String.fromCharCode(65 + n).toLowerCase();
-// const letterToNumber = (s) => s.toLowerCase().charCodeAt(0) - 97 + 1;
 
 const rpn = (csv) => {
-  let result = [];
   const COORD = makeCoordinates( csv );
-
-  csv
+  const result = csv
     .split('\n')
     .map( (line, i) => {
-      line
+      return line
         .split(',')
         .map( (cell, j) => {
           const address = `${numberToLetter(j)}${i + 1}`;
-          result.push(resolvePostfix(cell, address, COORD));
-      });
-      result.push('\n');// maybe detect change in the object key
-  });
-  // Object.entries(COORD).map(([ address, value ]) => {
-  //   console.log(address);
+          return resolvePostfix(cell, address, COORD);
+      }).join(',');
+  }).join('\n');
 
-  //   result.push(resolvePostfix(value, address, COORD));
-  // });
-
-  return result.toString();
+  return result;
 }
 
-// rpn('b1 b2 +,2 b2 3 * -, ,+\na1     ,5         , ,7 2 /\nc2 3 * ,1 2       , ,5 1 2 + 4 * + 3 -');
+const newCoord = { a1: 'b1 b2 +',
+b1: '2 b2 3 * -',
+c1: ' ',
+d1: '+',
+a2: 'a1     ',
+b2: '5         ',
+c2: ' ',
+d2: '7 2 /',
+a3: 'c2 3 * ',
+b3: '1 2       ',
+c3: ' ',
+d3: '5 1 2 + 4 * + 3 -' };
+resolvePostfix('b1 b2 +', 'a1', newCoord)
 
 module.exports = {
 	resolvePostfix,
   rpn,
   makeCoordinates,
+  isOperator,
+  isNumber,
+  isCoordenates,
 }
-
-
-// row numbers
-// column letter b2 = 5
-
-// CSV format for lines
-// \n => Mac
-// \n\r => PC
